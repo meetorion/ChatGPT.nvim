c951c2a868b20e4f100792258bf85ec63a28b020
zhuoying.li <zhuoying.li@transwarp.io>
2024-02-07 10:57:04 +0800

    修改为使用gpt4,并使用中文回复

--- lua/chatgpt/config.lua
+++ lua/chatgpt/config.lua
@@ -156,7 +156,7 @@ function M.defaults()
       },
     },
     openai_params = {
-      model = "gpt-3.5-turbo",
+      model = "gpt-4",
       frequency_penalty = 0,
       presence_penalty = 0,
       max_tokens = 300,
@@ -165,7 +165,7 @@ function M.defaults()
       n = 1,
     },
     openai_edit_params = {
-      model = "gpt-3.5-turbo",
+      model = "gpt-4",
       frequency_penalty = 0,
       presence_penalty = 0,
       temperature = 0,
